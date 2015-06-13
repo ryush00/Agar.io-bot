5e1d88567a8669ddccf975319abaee916ff2a70d
SeongHoon Ryu <ryush00@gmail.com>
2015-06-13 23:01:49 +0900

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -884,7 +884,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  originalName = "NotReallyABot",
+  originalName = "낙타",
   sessionScore = 0,
   serverIP = "",
   interNodes = [],
